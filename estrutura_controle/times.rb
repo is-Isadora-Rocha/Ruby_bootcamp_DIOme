f203476084f073793c961f158268b2b741885fbc
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-21 15:04:57 -0300

    times

--- estrutura_controle/times.rb
+++ estrutura_controle/times.rb
@@ -1,18 +1,21 @@
 # times = vezes, executa uma repetição por determinado número de vezes
 
+#ex: 1 print 
+6.times {print "Oi "}
+# Oi Oi Oi Oi Oi Oi / na mesma linha
 
+#ex: 1 puts 
+6.times {puts "Oi"}
+#Oi / Enter em casa linha
+#Oi
+#...
 
+#ex: 2 puts
+4.times do 
+    puts "Oi"
+end
 
-
-
-
-
-
-
-
-
-
-# times = vezes, executa uma repetição por determinado número de vezes
-# times = vezes, executa uma repetição por determinado número de vezes
-def index
+#ex: 2 print
+4.times do 
+    print "Oi"
 end
