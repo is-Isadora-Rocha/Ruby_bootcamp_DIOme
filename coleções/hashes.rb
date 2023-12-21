5c4522c1d30dad7cc1d79c13a2f34911550c2757
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-21 17:35:17 -0300

    adicionando um novo elemento hash

--- coleções/hashes.rb
+++ coleções/hashes.rb
@@ -5,3 +5,5 @@
 #INICIAR com pares CHAVE-VALOR variavel = {chave: 'valor', chave: 'valor'}
 
 #ADICIONAR um item ao hash = var[:nome inclusão] = "valor a ser add"
+#irb(main):004> animais
+# => {:ave=>"Tucano", :mamifero=>"Cachorro", :reptil=>"Lagarto", :aves2=>"Papaguaio"}
