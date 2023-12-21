e99a75343d2ba87c9f800200ad956459e251184a
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-21 17:29:09 -0300

    texto

---
+++ coleções/hashes.rb
@@ -0,0 +1,7 @@
+#lista que tem CHAVE: VALOR
+
+#CRIAR um hash = Hash.new
+
+#INICIAR com pares CHAVE-VALOR variavel = {chave: 'valor', chave: 'valor'}
+
+#ADICIONAR um item ao hash = var[:nome inclusão] = "valor a ser add"
