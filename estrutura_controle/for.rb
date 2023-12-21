aeed70f2ff9fefa07215c23e19bd1426d58b178e
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-21 14:19:40 -0300

    while e for

--- estrutura_controle/for.rb
+++ estrutura_controle/for.rb
@@ -1,1 +1,14 @@
 # for = durante - usado para percorrer uma coleção de elementos
+#leia uma lista (array) de elementos - linguagens de programação
+
+#lista
+linguagens = ['Ruby', 'Go', 'C++', 'Java']
+count = 1
+
+#printar a lista na tela (puts)
+#laço for
+for linguagem in linguagens
+    puts linguagem
+    puts count
+    count = count + 1
+end
---
+++ estrutura_controle/while.rb
@@ -0,0 +1,11 @@
+#comparar um numero com uma condição
+num = 1
+
+#printar todos os numeros a partir de 1 e < 100
+#execute esse código ENQUANTO (WHILE), num for < 100
+
+while num < 100
+  puts num
+  #pegar o num e seguir ao próximo e incrementar 
+  num += 1
+end
