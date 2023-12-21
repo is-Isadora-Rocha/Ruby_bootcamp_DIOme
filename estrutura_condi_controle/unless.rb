4b709ccdb0085a715798c57ad506bb1ed8a5beb5
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-21 11:43:36 -0300

    case

---
+++ estrutura_condi_controle/unless.rb
@@ -0,0 +1,18 @@
+# if (se) = true 
+# else (senão) = if(false)
+# elsif = quando for necessário verificar mais uma condição if
+# Unless (a não ser que) = executado quando a confição for FALSE (if-not)
+
+x = 10
+unless x > 15 # -> a menos que x seja maior que 15
+    puts "x é menor ou igual a 15"
+else
+    puts "x é maior que 15"
+end 
+
+x = 20
+unless x > 15 # -> a menos que x seja maior que 15
+    puts "x é menor ou igual a 15"
+else
+    puts "x é maior que 15"
+end 
