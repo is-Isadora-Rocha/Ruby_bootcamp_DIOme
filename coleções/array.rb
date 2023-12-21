370c9888753b323c7c641ac59180554259a64710
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-21 16:53:22 -0300

    array

---
+++ coleções/array.rb
@@ -0,0 +1,16 @@
+livros = []
+
+# ADICIONAR um item no array = nome.push('item que queremos colocar')
+# => livros.push('O Idiota de Dostoievski')
+# => livros.push('Clean Code', 'Vidas Secas')
+
+#ORGANIZAR os novos dados da lista = nome.insert(0, 'nome1', 'nome2')
+# => livros.insert(0, 'Dom Casmurro','Sargento Milicias')
+# => ["Dom Casmurro", "Sargento Milicias", "Entendendo Algoritmos", "O Idiota de Dostoievski", "Clean Code", "Vidas Secas"]
+
+# NA POSIÇÃO 2
+# => livros.insert(2, 'Ubirajara')
+
+#ACESSAR os elementos da lista = nome[numero]
+# => livros[1..3]
+# => ["Sargento Milicias", "Ubirajara", "Entendendo Algoritmos"]
