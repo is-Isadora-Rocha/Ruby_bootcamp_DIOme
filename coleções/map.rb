7e9142c70caa33a7967128585722481a0685b137
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-22 09:57:23 -0300

    map

--- coleções/map.rb
+++ coleções/map.rb
@@ -1,1 +1,17 @@
 #criar um array baseado em outro array existente
+
+numeros = [2,3,4,5]
+
+# .map não altera o conteúdo do array original
+novo_numeros = numeros.map do |x|
+   x * 5
+end 
+
+puts "\nArray Original: \n#{numeros}"
+puts "\nNovo Array: \n#{novo_numeros}"
+
+# Array Original:
+# [2, 3, 4, 5]
+
+# Novo Array:
+# [10, 15, 20, 25]
