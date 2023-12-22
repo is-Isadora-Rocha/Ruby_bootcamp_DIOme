8495ecb2abe3dc87bea727d05a890359e4dab5e2
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-22 09:44:21 -0300

    each com array e hash

--- coleções/each.rb
+++ coleções/each.rb
@@ -6,7 +6,21 @@
 #CRIAR array
 nomes = ['Maria', 'João', 'Marcelo']
 
+nome = 'Mariana' #'Mariana' não está dentro de nomes
+
 #pega cada nome que está dentro de nomes, e imprima
 nomes.each do |nome|
 puts nome
 end
+puts nome 
+#aqui ele vai imprimir 'Mariana', pois está fora do array
+
+
+#EACH com HASH = par ordenado 
+cursos = {'Curso 1' => 'Ruby', 'Curso 2' => 'Go', 'Curso 3' => 'Python', 'Curso 4' => 'Java'}
+
+cursos.each do |key,value|
+    puts "#{key} #{value}"
+end
+
+
