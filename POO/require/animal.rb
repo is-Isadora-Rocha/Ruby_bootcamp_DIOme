f984a633199549a84b420be326c9b5444c0b33d2
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-26 17:36:03 -0300

    require

---
+++ POO/require/animal.rb
@@ -0,0 +1,13 @@
+class Animal
+    def dormir
+        puts 'Zzzzzzz'
+    end
+
+    def pular
+        puts 'tóin, toín'
+    end
+end
+
+animal = Animal.new
+animal.pular
+animal.dormir
---
+++ POO/require/import.rb
@@ -0,0 +1,14 @@
+# require './animal.rb'
+
+# sempre utilizar a ordem da herança!! 
+
+require_relative './animal.rb' 
+#\POO\require> cd ..
+#\POO> ruby require/import.rb
+
+require_relative './gato.rb'
+
+
+# ...\require> ruby import.rb
+# tóin, toín
+# Zzzzzzz
