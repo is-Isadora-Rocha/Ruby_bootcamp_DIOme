ce70d89311b4aed839d6b4c460af1b89b95e06c0
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-12-29 10:45:48 -0300

    var global

--- POO/global.rb
+++ POO/global.rb
@@ -1,3 +1,21 @@
+# pode ser acessada de qualquer lugar do programa. 
+
 class Teste
-    
+    def global
+        $global = 0
+        puts $global
+    end
 end 
+
+class Teste2
+    def other_global
+        $global += 1
+        puts $global
+    end
+end
+
+teste = Teste.new
+teste2 = Teste2.new
+teste.global
+teste2.other_global
+puts $global
