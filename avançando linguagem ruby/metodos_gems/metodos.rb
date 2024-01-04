f359d81cd00718653fa646e2cac84ac85b68c621
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-01-04 13:49:28 -0300

    almoco

--- avançando linguagem ruby/metodos_gems/metodos.rb
+++ avançando linguagem ruby/metodos_gems/metodos.rb
@@ -5,16 +5,30 @@
 # # Reciclar seções de código em vez de repeti-las em todos os programas pode torná-lo mais legível e gerenciável.
 
 #CRIANDO UM MÉTODO
-def linguagem
-    "Ruby!"
+# def linguagem
+#     "Ruby!"
+# end
+
+# puts linguagem
+
+# def nome(nome,sobrenome)
+#     puts "Oi, #{nome} #{sobrenome} é o meu nome, e estou aprendendo " + linguagem
+# end
+
+# nome = 'Isadora'
+# sobrenome = 'Rocha'
+# nome(nome,sobrenome)
+
+# OUTRO EXEMPLO em prática
+def almoco (carbo, prot, gord)
+    puts "Sua fonte de carboidrato é #{carbo}, sua proteína é #{prot} e sua fonte de gordura é #{gord}\nBom apetite!"
 end
 
-puts linguagem
+puts "Qual será o seu carboidrato?"
+carbo = gets.chomp
+puts "Qual será sua proteína?"
+prot = gets.chomp
+puts "E sua fonte de gordura?"
+gord = gets.chomp
 
-def nome(nome,sobrenome)
-    puts "Oi, #{nome} #{sobrenome} é o meu nome, e estou aprendendo " + linguagem
-end
-
-nome = 'Isadora'
-sobrenome = 'Rocha'
-nome(nome,sobrenome)
+puts almoco(carbo, prot, gord)
