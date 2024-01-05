74400351fe999c47b6cb90b6a1c20fd5fdac4494
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-01-05 09:52:04 -0300

    outro metodo

--- avançando linguagem ruby/metodos_gems/metodos.rb
+++ avançando linguagem ruby/metodos_gems/metodos.rb
@@ -5,19 +5,19 @@
 # # Reciclar seções de código em vez de repeti-las em todos os programas pode torná-lo mais legível e gerenciável.
 
 #CRIANDO UM MÉTODO
-# def linguagem
-#     "Ruby!"
-# end
+def linguagem
+    "Ruby!"
+end
 
-# puts linguagem
+puts linguagem
 
-# def nome(nome,sobrenome)
-#     puts "Oi, #{nome} #{sobrenome} é o meu nome, e estou aprendendo " + linguagem
-# end
+def nome(nome,sobrenome)
+    puts "Oi, #{nome} #{sobrenome} é o meu nome, e estou aprendendo " + linguagem
+end
 
-# nome = 'Isadora'
-# sobrenome = 'Rocha'
-# nome(nome,sobrenome)
+nome = 'Isadora'
+sobrenome = 'Rocha'
+nome(nome,sobrenome)
 
 # OUTRO EXEMPLO em prática
 def almoco (carbo, prot, gord)
@@ -32,3 +32,18 @@ puts "E sua fonte de gordura?"
 gord = gets.chomp
 
 puts almoco(carbo, prot, gord)
+
+sum = 0
+numbers = [5, 10, 5]
+
+numbers.each do |number|
+  sum += number
+end
+
+puts sum
+
+divisor = 0
+numbers2 = [30, 6, 3]
+
+numbers2.each {|number| divisor += number }
+puts divisor
